f82157bd74dffed09b79a0260433b51c959b1538
Neengash <pepmanas93@gmail.com>
2022-12-14 20:44:30 +0100

    Updated FeTo version (fixed SO_Architecture errors) and reflected changes on game scripts

---
+++ Assets/Scripts/FeToUpgrade/ManualGameEventListener.cs
@@ -0,0 +1,12 @@
+using FeTo.SOArchitecture;
+
+public class ManualGameEventListener : GameEventListener
+{
+    protected new void OnEnable() {
+        //Event.RegisterListener(this);
+    }
+
+    protected new void OnDisable() {
+        //Event.UnregisterListener(this);
+    }
+}
